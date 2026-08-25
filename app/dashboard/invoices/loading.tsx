88f1d1a5a2b9e9cb99f15c75a99d877200b52b2b
Hassan <hassanjavaid8099@gmail.com>
2026-08-25 11:24:05 +0500

    Add dashboard section loading states

---
+++ app/dashboard/invoices/loading.tsx
@@ -0,0 +1,5 @@
+import { DashboardLoader } from "../../_components/loading-indicator";
+
+export default function InvoicesLoading() {
+  return <DashboardLoader label="Loading invoices" />;
+}
---
+++ app/dashboard/profile/loading.tsx
@@ -0,0 +1,5 @@
+import { DashboardLoader } from "../../_components/loading-indicator";
+
+export default function ProfileLoading() {
+  return <DashboardLoader label="Loading company profile" />;
+}
